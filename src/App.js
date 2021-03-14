47b165a73007986b011bbe30d6241ea3fa05262f
chmh0805 <chmh0805@gmail.com>
2021-03-14 18:13:20 +0900

    #2.2 map을 활용한 동적 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React from "react";
 
-const Food = ({ fav }) => {
-  return <h1>I like {fav}</h1>;
+const Food = (props) => {
+  return (
+    <div>
+      <h2>I like {props.name}</h2>
+      <img src={props.picture} style={{ width: "200px", height: "200px" }} />
+    </div>
+  );
 };
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+  },
+  {
+    name: "삼겹살",
+    image:
+      "https://recipe1.ezmember.co.kr/cache/recipe/2020/06/30/d77f639c047a6cf6d5afb463d9d210ce1.jpg",
+  },
+  {
+    name: "부대찌개",
+    image:
+      "https://recipe1.ezmember.co.kr/cache/recipe/2018/02/27/b5806f01ccb6f6d5d9aa94bdd6f4287e1_m.jpg",
+  },
+  {
+    name: "된장찌개",
+    image:
+      "https://recipe1.ezmember.co.kr/cache/recipe/2016/10/27/462f2e2c70a260e044c4450bc18e6f651_m.jpg",
+  },
+];
 
 const App = () => {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukumi" />
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 };
